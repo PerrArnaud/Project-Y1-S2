5f946d8fdc1f3c59b4236dc29d2b42715b6ffb5c
ArnaudPer <mr.arnaud.perrier@gmail.com>
2025-02-13 15:02:01 +0100

    explosion

--- www/pages/formulaire.php
+++ www/pages/formulaire.php
@@ -9,7 +9,7 @@
               <input type="text" name="username" id="username" required placeholder="Username" > </br> </br> 
               Mot de passe <br>
               <input  type="password" name="secret" id="secret" placeholder="Password" minlength="14" required> </br> </br> </br>
-              <input type="submit" id="send" value="Se connecter">
+              <input type="submit" value="Se connecter">
             </form>
         </div>
         <div id="result">
@@ -24,8 +24,8 @@ if (!empty($_POST)) {
     $dbname = "phpsql";
     $username = "root";
     $password = "rootpassword";
-    $PDO = new PDO(dsn: "mysql:host=$host;dbname=$dbname", username: $username, password: $password);
-    $sqlc = $PDO->prepare("INSERT INTO connexion (username, pass) VALUES (?,?)");
+    $PDO = new PDO(dsn:"mysql:host=$host;dbname=$dbname", username:$username, password:$password);
+    $sqlc = $PDO->prepare("INSERT INTO connexion (username,pass) VALUES (?,?)");
     $sqlc->execute([$user,$hashedPassword]);
 }
 ?>
