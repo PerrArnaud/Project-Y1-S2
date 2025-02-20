d7c96e43da5aab3d8616dfa82fc2f08bb5596e04
ArnaudPer <mr.arnaud.perrier@gmail.com>
2025-02-20 14:37:00 +0100

    page name

---
+++ www/pages/signin.php
@@ -0,0 +1,31 @@
+<html>
+    <head>
+        <link href="../assets/css/style.css" rel=stylesheet>
+    </head>
+    <body>
+        <div id="formulaire">
+            <form   method="Post">
+                Identifiant </br>
+              <input type="text" name="username" id="username" required placeholder="Username" > </br> </br> 
+              Mot de passe <br>
+              <input  type="password" name="secret" id="secret" placeholder="Password" minlength="14" required> </br> </br> </br>
+              <input type="submit" value="Se connecter">
+            </form>
+        </div>
+        <div id="result">
+        </div>
+    </body>
+</html>
+<?php
+$host="lamp_mysql";
+$dbname = "phpsql";
+$userroot = "root";
+$passroot = "rootpassword";
+$PDO = new PDO("mysql:host=$host;dbname=$dbname", $userroot, $passroot);
+if (!empty($_POST)) {
+    $hashedPassword=password_hash($_POST['secret'], PASSWORD_DEFAULT);
+    $user = $_POST['username'];
+    $sqlc = $PDO->prepare("INSERT INTO connexion (username,pass) VALUES (?,?)");
+    $sqlc->execute([$user,$hashedPassword]);
+}
+?>
