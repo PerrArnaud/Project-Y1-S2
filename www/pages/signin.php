515a11a963f3121b22815ad24689533486a9d35b
ArnaudPer <mr.arnaud.perrier@gmail.com>
2025-03-17 14:03:07 +0100

    Finished sign in maybe

--- www/pages/signin.php
+++ www/pages/signin.php
@@ -8,7 +8,9 @@
                 Identifiant </br>
               <input type="text" name="username" id="username" required placeholder="Username" > </br> </br> 
               Mot de passe <br>
-              <input  type="password" name="secret" id="secret" placeholder="Password" minlength="14" required> </br> </br> </br>
+              <input  type="password" name="secret" id="secret" placeholder="Password" minlength="14" required> </br> </br> 
+              Confirmer le mot de passe
+              <input type="password" name="confirm" id="secret2" placeholder="Confirm password" minlength="14" required> </br> </br> </br>
               <input type="submit" value="Se connecter">
             </form>
         </div>
@@ -25,7 +27,12 @@ $PDO = new PDO("mysql:host=$host;dbname=$dbname", $userroot, $passroot);
 if (!empty($_POST)) {
     $hashedPassword=password_hash($_POST['secret'], PASSWORD_DEFAULT);
     $user = $_POST['username'];
-    $sqlc = $PDO->prepare("INSERT INTO connexion (username,pass) VALUES (?,?)");
-    $sqlc->execute([$user,$hashedPassword]);
+    if ($_POST['secret'] == $_POST['confirm']){
+        $sqlc = $PDO->prepare("INSERT INTO connexion (username,pass) VALUES (?,?)");
+        $sqlc->execute([$user,$hashedPassword]);
+    }
+    else{
+        echo("Le mot de passe n'est pas identique");
+    }
 }
 ?>
