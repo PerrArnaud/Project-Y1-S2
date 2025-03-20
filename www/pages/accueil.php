ca8dd65c9c89379ec7bd07df9b31f3230f3f127f
ArnaudPer <mr.arnaud.perrier@gmail.com>
2025-03-20 09:54:54 +0100

    Bouton Deconnexion

--- www/pages/accueil.php
+++ www/pages/accueil.php
@@ -1,11 +1,27 @@
 <?php
-session_start(); //Continue la session.
+session_start(); // Continue la session.
 
-if (!isset($_SESSION['username'])) {//Permet d'empêcher un accès à la page si la personne n'est pas connectée.
+if (!isset($_SESSION['username'])) { // Vérifie si l'utilisateur est connecté.
     echo "Vous n'êtes pas connecté.";
     exit();
 }
-
-echo "Bienvenue, " . htmlspecialchars($_SESSION['username']) . "!"; //Message d'accueil avec le nom d'utilisateur entré.
 ?>
-<a href="logout.php">Déconnexion</a>
+
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Accueil</title>
+    <link href="../assets/css/style_a.css" rel="stylesheet"> <!-- Lien vers le fichier CSS externe -->
+</head>
+<body>
+
+    <p>Bienvenue, <?php echo htmlspecialchars($_SESSION['username']); ?> !</p>
+
+    <div class="logout-container">
+        <a href="logout.php">Déconnexion</a>
+    </div>
+
+</body>
+</html>
