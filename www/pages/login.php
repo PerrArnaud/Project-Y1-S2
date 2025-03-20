6c9ae35a55d7b84e8de128443574ef2c6066d87c
ArnaudPer <mr.arnaud.perrier@gmail.com>
2025-03-20 08:36:26 +0100

    header

--- www/pages/login.php
+++ www/pages/login.php
@@ -1,21 +1,3 @@
-<html>
-    <head>
-        <link href="../assets/css/style.css" rel=stylesheet>
-    </head>
-    <body>
-        <div id="formulaire">
-            <form   method="Post">
-                Identifiant </br>
-              <input type="text" name="username" id="username" required placeholder="Username" > </br> </br> 
-              Mot de passe <br>
-              <input  type="password" name="secret" id="secret" placeholder="Password" minlength="14" required> </br> </br> </br>
-              <input type="submit" value="Se connecter">
-            </form>
-        </div>
-        <div id="result">
-        </div>
-    </body>
-</html>
 <?php
 $host = "lamp_mysql";
 $dbname = "phpsql";
@@ -42,8 +24,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         $row = $stmt->fetch(PDO::FETCH_ASSOC);
 
         if ($row && password_verify($password, $row['pass'])) { //Vérifie si le mot de passe entré est le même que dans la base de données.
-            echo "<div id='result'>Connexion réussie !</div>";
-            header('Location : http://localhost:8080/pages/accueil.php'); //Redirige l'utilisateur.
+            header('Location: http://localhost:8080/pages/accueil.php'); //Redirige l'utilisateur.
             exit();
         } else {
             echo "<div id='result'>Identifiant ou mot de passe incorrect.</div>";
@@ -53,3 +34,21 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     }
 }
 ?>
+<html>
+    <head>
+        <link href="../assets/css/style.css" rel=stylesheet>
+    </head>
+    <body>
+        <div id="formulaire">
+            <form   method="Post">
+                Identifiant </br>
+              <input type="text" name="username" id="username" required placeholder="Username" > </br> </br> 
+              Mot de passe <br>
+              <input  type="password" name="secret" id="secret" placeholder="Password" minlength="14" required> </br> </br> </br>
+              <input type="submit" value="Se connecter">
+            </form>
+        </div>
+        <div id="result">
+        </div>
+    </body>
+</html>
